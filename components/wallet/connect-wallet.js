7c3facd43006767f9b64470aab37205719523473
Juan Cruz Bello Vargas <jcbellovargas@gmail.com>
2022-09-05 10:48:31 -0300

    Etherscan link refactor

--- components/wallet/connect-wallet.js
+++ components/wallet/connect-wallet.js
@@ -16,15 +16,17 @@ export default function ConnectWallet(props) {
       )}
       {props.isWalletConnected && (
         <div className="my-5">
-            <div class="stat bg-base-100 rounded-lg bg-opacity-30">
-              <div class="stat-title">Address</div>
-              <div class="stat-value text-secondary">{scrubAddress(selectedAddress())}</div>
-              <div class="stat-desc text-lg">
-                <a href={"https://goerli.etherscan.io/address/"+selectedAddress()} target="blank">
-                  <span class="float-left">Ver en Etherescan</span>
-                  <img src="external-link.svg" class="ml-39 mt-1 w-5 h-4" alt="Etherscan"/>
+          <h3 className="text-lg font-bold">Wallet conectada</h3>
+            <div class="stat bg-base-100 rounded-lg bg-opacity-30 pl-1">
+              <div class="stat-title text-2xl">Address</div>
+              <div class="stat-value text-5xl text-secondary">{scrubAddress(selectedAddress())}</div>
+              <div class="stat-desc text-xl">
+                <a href={"https://goerli.etherscan.io/address/"+selectedAddress()} class="justify-items-center" target="blank">
+                  <div class="flex mt-3">
+                    <img src="external-link.svg" class="mt-2 ml-0 w-5 h-4" alt="Etherscan"/>
+                    <span class="float-left">Ver en Etherescan</span>
+                  </div>
                 </a>
-
               </div>
             </div>
             <div className="stats bg-primary text-primary-content">
