0e6a2b5bf358fd494808eae304e9a5e1f5b50712
Juan Cruz Bello Vargas <jcbellovargas@gmail.com>
2022-09-26 15:42:28 -0300

    next-auth initial config

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react"
 import Layout from '../components/layout'
 import AccountContext from '/contexts/accountContext'
 import { useState } from 'react'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ 
+  Component,
+  pageProps: { session, ...pageProps}
+}) {
 
   const [address, setAddress] = useState(null);
 
   return (
     <AccountContext.Provider value={{address, setAddress}}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <SessionProvider session={session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </AccountContext.Provider>
-      
+    
   )
 }
