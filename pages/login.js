41ee10b85060efc8b525dbe8e344bc34913516ea
Juan Cruz Bello Vargas <jcbellovargas@gmail.com>
2022-10-11 13:16:53 -0300

    Login errors handling

--- pages/login.js
+++ pages/login.js
@@ -5,7 +5,7 @@ export default function Login({ providers }) {
   const { error } = useRouter().query;
 
   const providerLogo = (id) => {
-    switch(id) {
+    switch (id) {
       case 'google':
         return <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>;
       case 'twitter':
@@ -19,6 +19,10 @@ export default function Login({ providers }) {
     }
   }
 
+  const loginError = () => {
+    !!error && (error != "SessionRequired")
+  }
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col">
@@ -28,7 +32,7 @@ export default function Login({ providers }) {
         </div>
         <div className="card flex-shrink-0 w-full max-w-2xl shadow-2xl bg-base-100">
           <div className="card-body">
-            {error && (
+            {loginError() && (
               <div className="alert alert-error shadow-lg">
                 <div>
                   <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
@@ -57,7 +61,7 @@ export async function getServerSideProps(context) {
   let authProviders = []
   try {
     authProviders = await getProviders(context);
-  } catch (err){
+  } catch (err) {
     console.log(err)
   }
   return {
