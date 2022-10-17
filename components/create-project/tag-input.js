a7cdb42ee78a7039b8aab1213184d1c75880ff44
Juan Cruz Bello Vargas <jcbellovargas@gmail.com>
2022-10-17 17:10:41 -0300

    Create project form state

--- components/create-project/tag-input.js
+++ components/create-project/tag-input.js
@@ -2,40 +2,25 @@ import { useState } from 'react'
 import { WithContext as ReactTags } from 'react-tag-input-latest';
 
 
-export default function TagInput(){
-
-  const [tags, setTags] = useState([]);
-
-  const suggestions = [    
-    { id: 'Salud', text: 'Salud' },
-    { id: 'Tecnologia', text: 'Tecnologia' },
-    { id: 'Moda', text: 'Moda' },
-    { id: 'Negocios', text: 'Negocios' },
-    { id: 'Juegos', text: 'Juegos' },
-    { id: 'Arte', text: 'Arte' },
-    { id: 'Emergencia', text: 'Emergencia' },
-    { id: 'Comida', text: 'Comida' },
-    { id: 'Redes Sociales', text: 'Redes Sociales' },
-
-  ];
+export default function TagInput(props){
 
   const handleDelete = i => {
-    setTags(tags.filter((tag, index) => index !== i));
+    props.setTags(props.tags.filter((tag, index) => index !== i));
   };
 
   const handleAddition = newTag => {
-    const tagAlreadyExists = !!tags.find(t => t.id === newTag.id);
-    const newTags = tagAlreadyExists ? tags : [...tags, newTag]
-    setTags(newTags);
+    const tagAlreadyExists = !!props.tags.find(t => t.id === newTag.id);
+    const newTags = tagAlreadyExists ? props.tags : [...props.tags, newTag]
+    props.setTags(newTags);
   };
 
   return (
-    <ReactTags tags={tags}
-      suggestions={suggestions}
+    <ReactTags tags={props.tags}
+      suggestions={props.suggestions}
       handleDelete={handleDelete}
       handleAddition={handleAddition}
       minQueryLength={1}
-      placeholder={suggestions.map((s) => { return s.text })}
+      placeholder={props.suggestions.map((s) => { return s.text })}
       allowDeleteFromEmptyInput={false}
       inputFieldPosition="bottom"
       classNames={{
--- pages/new-project.js
+++ pages/new-project.js
@@ -1,27 +1,59 @@
 import { useSession } from "next-auth/react"
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 import TagInput from "../components/create-project/tag-input"
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { getSystemErrorName } from "util";
 
 
 
 export default function NewProject(){
   const { data: session } = useSession({required: true})
-  const [startDate, setStartDate] = useState(new Date());
-  const [imageUpload, setImageUpload] = useState({name: ""})
   const [displayImage, setDisplayImage] = useState("/image-placeholder.jpg")
+  const [projectImage, setProjectImage] = useState({name: ""})
+  const [projectName, setProjectName] = useState()
+  const [projectDescription, setProjectDescription] = useState()
+  const [projectTags, setProjectTags] = useState([])
+  const [projectFundingGoal, setProjectFundingGoal] = useState()
+  const [projectWallet, setProjectWallet] = useState()
+  const [projectDeadline, setProjectDeadline] = useState()
   const fileInputRef = useRef(null);
+
+  const tagOptions = [    
+    { id: 'Salud', text: 'Salud' },
+    { id: 'Tecnologia', text: 'Tecnologia' },
+    { id: 'Moda', text: 'Moda' },
+    { id: 'Negocios', text: 'Negocios' },
+    { id: 'Juegos', text: 'Juegos' },
+    { id: 'Arte', text: 'Arte' },
+    { id: 'Emergencia', text: 'Emergencia' },
+    { id: 'Comida', text: 'Comida' },
+    { id: 'Redes Sociales', text: 'Redes Sociales' },
+
+  ];
 
   const handleFileChange = (e) => {
     const file = e.target.files && e.target.files[0];
     if (!file) return;
-    setImageUpload(file);
+    // if (!isImageFile(file)) throw new Error("Not an image");
+    setProjectImage(file);
     setDisplayImage(URL.createObjectURL(file));
+  }
+
+  function isImageFile(file) {
+    return file && file['type'].split('/')[0] === 'image';
   }
 
   const handleImageInputClick = () => {
     fileInputRef.current.click()
+  }
+
+  const handleCreateButtonClick = () => {
+    alert([projectName, projectDescription, projectTags, projectFundingGoal, projectWallet, projectDeadline, projectImage])
+  }
+
+  const createButtonEnabled = () => {
+    return (projectName && projectDescription && projectFundingGoal && projectWallet && projectDeadline && projectImage)
   }
 
   return(
@@ -36,37 +68,37 @@ export default function NewProject(){
                   <label className="label">
                     <span className="label-text">Nombre del proyecto</span>
                   </label>
-                  <input type="text" defaultValue={""} placeholder="Nombre" className="input input-bordered input-primary w-full" onChange={() => {}} />
+                  <input type="text" placeholder="Nombre" className="input input-bordered input-primary w-full" onChange={(e) => {setProjectName(e.target.value)}} />
                 </div>
                 <div className="form-control w-full max-w-2xl">
                   <label className="label">
                     <span className="label-text">Descripcion</span>
                   </label>
-                  <textarea className="textarea textarea-primary" placeholder="Detalles del proposito del proyecto y sus caracteristicas"></textarea>
+                  <textarea className="textarea textarea-primary" onChange={(e) => setProjectDescription(e.target.value)} placeholder="Detalles del proposito del proyecto y sus caracteristicas"></textarea>
                 </div>
                 <div className="form-control w-full max-w-2xl">
                   <label className="label">
                     <span className="label-text">Tipo de proyecto</span>
                   </label>
-                  <TagInput/>
+                  <TagInput suggestions={tagOptions} tags={projectTags} setTags={setProjectTags}/>
                 </div>
                 <div className="form-control w-full max-w-2xl">
                   <label className="label">
                     <span className="label-text">Objetivo a recaudar (USDT)</span>
                   </label>
-                  <input type="number" defaultValue={""} placeholder="Monto objetivo en USDT" className="input input-bordered input-primary w-full" onChange={() => {}}/>
+                  <input type="number" placeholder="Monto objetivo en USDT" className="input input-bordered input-primary w-full" onChange={(e) => {setProjectFundingGoal(e.target.value)}}/>
                 </div>
                 <div className="form-control w-full max-w-2xl">
                   <label className="label">
                     <span className="label-text">Wallet address del proyecto</span>
                   </label>
-                  <input type="text" defaultValue={""} placeholder="Wallet en la que se cobraran las recaudaciones" className="input input-bordered input-primary w-full" onChange={() => {}}/>
+                  <input type="text" onChange={(e) => {setProjectWallet(e.target.value)}} placeholder="Wallet en la que se cobraran las recaudaciones" className="input input-bordered input-primary w-full"/>
                 </div>
                 <div className="form-control w-full max-w-2xl">
                   <label className="label">
                     <span className="label-text">Fecha limite</span>
                   </label>
-                  <DatePicker className="input input-bordered input-primary w-full" selected={startDate} onChange={(date) => setStartDate(date)} />
+                  <DatePicker className="input input-bordered input-primary w-full" onChange={(date) => setProjectDeadline(date)} selected={projectDeadline} placeholderText={"Fecha limite para alcanzar el objetivo"}/>
                 </div>
 
                 <div className="form-control w-full max-w-2xl">
@@ -74,11 +106,11 @@ export default function NewProject(){
                     <span className="label-text">Imagen del proyecto</span>
                   </label>
                   <input className="hidden" ref={fileInputRef} type="file" onChange={handleFileChange}/>
-                  <input type="text" value={imageUpload.name} placeholder="Tamaño recomendado de 640x480" className="input input-bordered input-primary w-full" onClick={handleImageInputClick}/>
+                  <input type="text" value={projectImage.name} placeholder="Tamaño recomendado de 640x480" className="input input-bordered input-primary w-full" onClick={handleImageInputClick}/>
                 </div>
 
                 <div className="card-actions justify-end">
-                  <button onClick={() => {}} className="btn btn-primary" disabled={false}>Crear Proyecto</button>
+                  <button onClick={handleCreateButtonClick} className="btn btn-primary" disabled={!createButtonEnabled()}>Crear Proyecto</button>
                 </div>
               </div>
             </div>
@@ -86,7 +118,6 @@ export default function NewProject(){
           </div>
         )}
       </div>
-      
     </>
   )
 }
