1d6e82d800e12718440813a06223b1ccf92f5071
Juan Cruz Bello Vargas <jcbellovargas@gmail.com>
2022-10-20 17:45:34 -0300

    Fix project description length

--- components/explorar/project-card.js
+++ components/explorar/project-card.js
@@ -1,17 +1,24 @@
 import Link from 'next/link'
 
 export default function ProjectCard(props) {
+
+  const projectDesc = (description) => {
+    if(description.length < 140) return description;
+
+    return description.slice(0,140) + "..."
+  }
+
   return(
     <>
       <Link href={`/details/${props.project.id}`}>
         <div className="card w-3/4 bg-base-100 shadow-xl transition duration-100 hover:scale-105 hover:bg-white-600 cursor-pointer">
-          <figure><img src={props.project.image} alt="Img" className="w-[640px] h-[280px]" /></figure>
+          <figure><img src={props.project.image} alt="Img" className="w-[640px] h-[240px]" /></figure>
           <div className="card-body">
             <h2 className="card-title">
               {props.project.name}
               <div className="badge badge-secondary">NUEVO</div>
             </h2>
-            <p>{props.project.description}</p>
+            <p className="h-20">{projectDesc(props.project.description)}</p>
             <div className="flex justify-between">
               <span className="text-lg font-medium text-success dark:text-white">{`${props.project.currentBalance.amount} ${props.project.currentBalance.token}`}</span>
               <span className="text-lg font-medium text-success dark:text-white">{`${props.project.fundingGoalProgress}%`}</span>
